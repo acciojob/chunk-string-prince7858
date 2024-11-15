44bdb1cf4129492ce809a89382bf51d004633864
Prince Kumar <91559607+prince7858@users.noreply.github.com>
2024-11-15 16:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function stringChop(str, size) {
-	if(str == null || str.length == 0){
-		return []
-	}
-	let ans = []
-	while(str.length){
-		let temp = str.slice(i,size+i)
-		ans.push(temp)
-		i += size
-	}
-	return ans
-}
+    // your code here
+
+    if( str == null || str.length == 0)
+    {
+        return []
+    }
+
+    let ans = []
+    let i = 0
+
+    while(i < str.length)
+    {
+        let temp = str.slice(i,size + i)
+        ans.push(temp)
+        i += size
+    }
+
+    return ans
+
+  }
 
 // Do not change the code below
 const str = prompt("Enter String.");
